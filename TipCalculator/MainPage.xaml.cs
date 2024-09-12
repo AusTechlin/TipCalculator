b5435a936e15893a3eaaa5be27ba5732d08c31cd
Austin Techlin <techlinaustin@gmail.com>
2024-09-11 18:09:16 -0700

    Added TipCalculator

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace TipCalculator;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+        Title = "Tip Calculator Pro";
+    }
+
+
+    private void Calculate_OnClicked(object sender, EventArgs e)
+    {
+        var TipPercent = Double.Parse(((Button)sender).CommandParameter.ToString());
+        var strPercent = (TipPercent * 100).ToString();
+        Double dblAmount, dblTip, dblTotal;
+        Double.TryParse(txtAmount.Text, out dblAmount);
+        dblTip = TipPercent * dblAmount;
+        dblTotal = dblAmount + dblTip;
+        lblResult.Text = strPercent + "% Tip | " + dblTip.ToString("C2") + " Tip | Total " + dblTotal.ToString("C2");
+    }
+}
